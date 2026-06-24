0ea569a3805cd13b5c571c6eb33822ec23eba14e
azkaZubish <azkazubish@gmail.com>
2026-06-24 18:17:34 +0530

    Iteration 1 is done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 // Iteration 1: Names and Input
+let hacker1 = 'namjoon'
+let hacker2 = 'bts'
+
+console.log(`The driver name is ${hacker1}`);
+console.log(`The navigator name is ${hacker2}`);
 //
 // Iteration 2: Conditionals
 
